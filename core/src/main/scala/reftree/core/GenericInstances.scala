5df2b8a23f464250a30a11304bea42b860b95992
Nick Stanchenko <nick.stanch@gmail.com>
2016-11-10 11:37:38 -0800

    Add ToRefTree derivation for coproducts

--- core/src/main/scala/reftree/core/GenericInstances.scala
+++ core/src/main/scala/reftree/core/GenericInstances.scala
@@ -9,23 +9,47 @@ import shapeless._
  * exists or can be derived using this same facility.
  */
 trait GenericInstances {
-  implicit val `HNil RefTree`: ToRefTree[HNil] =
-    ToRefTree[HNil](RefTree.Ref(_, Seq.empty))
+  /** We use a different typeclass to avoid defining nonsense ToRefTree instances */
+  trait GenericToRefTree[-A] {
+    def refTree(value: A): RefTree
+  }
 
-  implicit def `HCons RefTree`[H, T <: HList](
-    implicit headAsTree: Lazy[ToRefTree[H]], tailAsTree: ToRefTree[T]
-  ): ToRefTree[H :: T] =
-    ToRefTree[H :: T] { value ⇒
-      RefTree.Ref(value, headAsTree.value.refTree(value.head) +: (value.tail.refTree match {
+  object GenericToRefTree {
+    def apply[A](toRefTree: A ⇒ RefTree) = new GenericToRefTree[A] {
+      def refTree(value: A) = toRefTree(value)
+    }
+  }
+
+  implicit val `Generic HNil RefTree`: GenericToRefTree[HNil] =
+    GenericToRefTree[HNil](RefTree.Ref(_, Seq.empty))
+
+  implicit def `Generic HCons RefTree`[H, T <: HList](
+    implicit headAsTree: Lazy[ToRefTree[H]], tailAsTree: GenericToRefTree[T]
+  ): GenericToRefTree[H :: T] =
+    GenericToRefTree[H :: T] { value ⇒
+      val head = headAsTree.value.refTree(value.head)
+      val tail = tailAsTree.refTree(value.tail) match {
         case RefTree.Ref(_, _, children, _, _) ⇒ children
         case x ⇒ Seq(x)
-      }))
+      }
+      RefTree.Ref(value, head +: tail)
     }
 
-  implicit def `Generic RefTree`[A <: AnyRef, L <: HList](
-    implicit generic: Generic.Aux[A, L], hListAsTree: Lazy[ToRefTree[L]]
+  implicit val `Generic CNil RefTree`: GenericToRefTree[CNil] =
+    GenericToRefTree[CNil](RefTree.Ref(_, Seq.empty))
+
+  implicit def `Generic CCons RefTree`[H, T <: Coproduct](
+    implicit headAsTree: Lazy[ToRefTree[H]], tailAsTree: GenericToRefTree[T]
+  ): GenericToRefTree[H :+: T] =
+    GenericToRefTree[H :+: T] {
+      case Inl(head) ⇒ headAsTree.value.refTree(head)
+      case Inr(tail) ⇒ tailAsTree.refTree(tail)
+    }
+
+  implicit def `Generic RefTree`[A <: AnyRef, R](
+    implicit generic: Generic.Aux[A, R], listAsTree: Lazy[GenericToRefTree[R]]
   ): ToRefTree[A] = ToRefTree[A] { value ⇒
-    hListAsTree.value.refTree(generic.to(value)) match {
+    listAsTree.value.refTree(generic.to(value)) match {
       case r: RefTree.Ref ⇒ RefTree.Ref(value, r.children)
       case x ⇒ x
     }
